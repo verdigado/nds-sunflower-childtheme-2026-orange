648ae14889c2eccf6dae0646575a4d89b93a3f56
Alexander Bigga <alexander.bigga@verdigado.com>
2026-02-23 21:49:30 +0100

    Fix php coding styles

--- functions.php
+++ functions.php
@@ -1,18 +1,45 @@
 <?php
-// Exit if accessed directly
-if ( !defined( 'ABSPATH' ) ) exit;
+/**
+ * Child Theme Configurator functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Child_Theme_Configurator
+ */
 
-if ( !function_exists( 'child_theme_configurator_css' ) ):
-    function child_theme_configurator_css() {
-        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'sunflower-style','sunflower-style' ) );
-    }
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! defined( 'SUNFLOWER_CHILD_THEME_VERSION' ) ) {
+	$sunflower_theme_data    = wp_get_theme( get_option( 'template' ) );
+	$sunflower_theme_version = $sunflower_theme_data->version;
+	define( 'SUNFLOWER_CHILD_THEME_VERSION', $sunflower_theme_version );
+}
+
+
+if ( ! function_exists( 'sunflower_child_theme_configurator_css' ) ) :
+	/**
+	 * Enqueue child theme styles.
+	 */
+	function sunflower_child_theme_configurator_css() {
+		wp_enqueue_style(
+			'sunflower_child_theme_configurator_css',
+			trailingslashit( get_stylesheet_directory_uri() ) . 'style.css',
+			array( 'sunflower-style' ),
+			SUNFLOWER_CHILD_THEME_VERSION
+		);
+	}
 endif;
-add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
+add_action( 'wp_enqueue_scripts', 'sunflower_child_theme_configurator_css', 10 );
 
 require_once 'functions/block-patterns.php';
 
-function child_theme_sunflower_setup() {
+/**
+ * Add editor styles.
+ */
+function sunflower_child_theme_sunflower_setup() {
 		add_editor_style( '/assets/css/editor-style.css' );
 }
 
-add_action( 'after_setup_theme', 'child_theme_sunflower_setup',10 );
+add_action( 'after_setup_theme', 'sunflower_child_theme_sunflower_setup', 10 );
--- functions/block-patterns.php
+++ functions/block-patterns.php
@@ -1,25 +1,36 @@
 <?php
-    $dirs = glob(get_stylesheet_directory() . '/functions/block-patterns/*', GLOB_ONLYDIR);
+/**
+ * Block patterns registration.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/child-themes/#enqueueing-stylesheets
+ *
+ * @package Child_Theme_Configurator
+ */
 
-    foreach($dirs AS $dir){
-        $basenameDir = basename($dir);
+/**
+ * Block patterns registration.
+ */
+$sunflower_dirs = glob( get_stylesheet_directory() . '/functions/block-patterns/*', GLOB_ONLYDIR );
 
-        register_block_pattern_category(
-            'hvba-nds-' . $basenameDir,
-            array( 'label' => esc_html__('NDS', 'hvba-nds') . '-' . ucfirst($basenameDir))
-        );
+foreach ( $sunflower_dirs as $sunflower_dir ) {
+	$sunflower_basename_dir = basename( $sunflower_dir );
 
-        $files = glob($dir . '/*.html');
-        foreach($files AS $file){
-            $basenameFile = basename($file, '.html');
+	register_block_pattern_category(
+		'hvba-nds-' . $sunflower_basename_dir,
+		array( 'label' => esc_html__( 'NDS', 'default' ) . '-' . ucfirst( $sunflower_basename_dir ) )
+	);
 
-            register_block_pattern(
-                'nds-sunflower-childtheme-2026-orange/' . $basenameFile,
-                array(
-                    'title'       => ucfirst($basenameFile),
-                    'categories'  => ['hvba-nds-' . $basenameDir],
-                    'content'     => file_get_contents($file),
-                )
-            );
-        }
-    }
+	$sunflower_files = glob( $sunflower_basename_dir . '/*.html' );
+	foreach ( $sunflower_files as $sunflower_file ) {
+		$sunflower_basename_file = basename( $sunflower_file, '.html' );
+
+		register_block_pattern(
+			'nds-sunflower-childtheme-2026-orange/' . $sunflower_basename_file,
+			array(
+				'title'      => ucfirst( $sunflower_basename_file ),
+				'categories' => array( 'hvba-nds-' . $sunflower_basename_dir ),
+				'content'    => wp_remote_get( $sunflower_file ),
+			)
+		);
+	}
+}
