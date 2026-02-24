f59bffe223aa258673ba6bc73bd794998ee65156
Alexander Bigga <alexander.bigga@verdigado.com>
2026-02-24 13:04:11 +0100

    Some css fixes and template part inclusion

--- functions/block-patterns.php
+++ functions/block-patterns.php
@@ -16,20 +16,21 @@ foreach ( $sunflower_dirs as $sunflower_dir ) {
 	$sunflower_basename_dir = basename( $sunflower_dir );
 
 	register_block_pattern_category(
-		'hvba-nds-' . $sunflower_basename_dir,
+		'nds-' . $sunflower_basename_dir,
 		array( 'label' => esc_html__( 'NDS', 'default' ) . '-' . ucfirst( $sunflower_basename_dir ) )
 	);
 
-	$sunflower_files = glob( $sunflower_basename_dir . '/*.html' );
+	$sunflower_files = glob( $sunflower_dir . '/*.html' );
 	foreach ( $sunflower_files as $sunflower_file ) {
-		$sunflower_basename_file = basename( $sunflower_file, '.html' );
+		$sunflower_basename_files = basename( $sunflower_file, '.html' );
 
 		register_block_pattern(
-			'nds-sunflower-childtheme-2026-orange/' . $sunflower_basename_file,
+			'nds-sunflower-childtheme-2026-orange/' . $sunflower_basename_files,
 			array(
-				'title'      => ucfirst( $sunflower_basename_file ),
-				'categories' => array( 'hvba-nds-' . $sunflower_basename_dir ),
-				'content'    => wp_remote_get( $sunflower_file ),
+				'title'      => ucfirst( $sunflower_basename_files ),
+				'categories' => array( 'nds-' . $sunflower_basename_dir ),
+				// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
+				'content'    => file_get_contents( $sunflower_file ),
 			)
 		);
 	}
